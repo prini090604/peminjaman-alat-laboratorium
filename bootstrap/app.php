67eb13fbc601551554590d2649e38a1e1c84c22c
prini090604 <priniprini009@gmail.com>
2026-05-13 20:46:46 +0700

    update bootstrap/app

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
+    ->withSchedule(function ($schedule) {
+        require base_path('routes/console.php');
+    })
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
             'role' => \App\Http\Middleware\RoleMiddleware::class,
